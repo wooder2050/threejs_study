14b1933f40366a08ddccb7570c536edfbbe650f6
jaeyoung Kang <jykang@superb-ai.com>
2023-06-20 15:41:05 +0900

    init: study 자료 업로드

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import example from "./sample";
+
+// import example from "./meshControl/position";
+// import example from "./meshControl/scale";
+// import example from "./meshControl/rotation";
+// import example from "./meshControl/group";
+// import example from './geometry/box';
+// import example from './geometry/position';
+// import example from './cameraControl/orbitControls';
+// import example from './cameraControl/trackballControls';
+// import example from './cameraControl/flyControls';
+// import example from './cameraControl/firstPersonControls';
+// import example from './cameraControl/pointerLockControls';
+// import example from './cameraControl/dragControls';
+// import example from './cameraControl/pointerLockControlsWithKeys';
+// import example from "./material/meshBasicMaterial"
+// import example from "./material/meshPhongMaterial"
+// import example from "./material/meshStandardMaterial"
+// import example from "./material/flatShading"
+// import example from "./material/side"
+// import example from "./material/texturesLoad"
+// import example from "./material/loadingManager"
+// import example from "./material/textureProps"
+// import example from "./material/textureCube"
+// import example from "./material/meshToonMaterial"
+// import example from "./material/meshNormalMaterial"
+// import example from "./material/meshMatcapMaterial"
+// import example from "./material/meshStandardMaterialEffect"
+// import example from "./material/environmentMap"
+// import example from "./material/skybox"
+// import example from "./material/skyboxWithEnvMap"
+// import example from "./material/canvasTexture"
+
+
+example();
